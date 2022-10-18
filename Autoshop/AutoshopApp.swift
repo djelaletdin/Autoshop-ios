7efd9030d248a5ad3361c51d2de223dcff8bd4fb
djelaletdin <jelaletdinov@gmail.com>
2022-10-18 23:03:05 +0900

    Initial Commit

---
+++ Autoshop/AutoshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoshopApp.swift
+//  Autoshop
+//
+//  Created by Didar Jelaletdinov on 2022/10/18.
+//
+
+import SwiftUI
+
+@main
+struct AutoshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
